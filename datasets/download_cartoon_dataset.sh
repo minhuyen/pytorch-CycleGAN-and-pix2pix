1c92a63d1afedfa8490b5393503e34611de96dab
Uyen Do <minhuyendo@gmail.com>
2020-10-29 16:12:15 +0700

    update download cartoon dataset

--- datasets/download_cartoon_dataset.sh
+++ datasets/download_cartoon_dataset.sh
@@ -1,6 +1,6 @@
 FILE=artwork-new
 # URL=https://www.dropbox.com/s/721bux0zsoih5j0/$FILE.zip?dl=1
-https://www.dropbox.com/s/ipwqry4yylc3vfs/$FILE.zip?dl=1
+URL=https://www.dropbox.com/s/ipwqry4yylc3vfs/$FILE.zip?dl=1
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
